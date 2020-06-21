67b10999c312409802638f3c21aad576ebdf47f1
Margarita Barbotiko <mbarbotiko@gmail.com>
2020-06-20 19:46:13 -0500

    changing stateless components to functional components

--- src/App.js
+++ src/App.js
@@ -68,83 +68,172 @@ class App extends Component {
   }
 }
 
-class Table extends Component {
-  render() {
-    const isSearched = searchTerm => {
-      return item => {
-        return item.title.toLowerCase().includes(searchTerm.toLowerCase());
-      }
+// class Table extends Component {
+//   render() {
+//     const isSearched = searchTerm => {
+//       return item => {
+//         return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+//       }
+//     }
+//     const { list, pattern, onDismiss } = this.props
+//     return (
+//       <div className='Results-Container'>
+//         {list.filter(isSearched(pattern)).map(item =>
+//           <div className='Results-Details' key={item.objectID} >
+
+//             <div>
+//               <img src={require(`${item.image}`)} alt={`Movie Poster: ${item.title}`} />
+//               <p>Movie Title : {item.title}</p>
+//             </div>
+//             <div>
+//               <p>Director : {item.director}</p>
+//             </div>
+//             <span>
+//               <Button
+//                 onClick={() => onDismiss(item.objectID)}
+//                 className='Button Dismiss'
+//               >
+//                 Dismiss
+//               </Button>
+//               {/* <Button //show difference of being able to pass className as props instead
+//                 onClick={() => onDismiss(item.objectID)}
+//                 className='Button'
+//               >
+//                 Dismiss
+//               </Button> */}
+
+//             </span>
+
+
+//           </div>
+
+//         )}
+
+//       </div>
+//     )
+//   }
+// }
+
+
+const Table = ({ list, pattern, onDismiss }) => {
+  const isSearched = searchTerm => {
+    return item => {
+      return item.title.toLowerCase().includes(searchTerm.toLowerCase());
     }
-    const { list, pattern, onDismiss } = this.props
-    return (
-      <div className='Results-Container'>
-        {list.filter(isSearched(pattern)).map(item =>
-          <div className='Results-Details' key={item.objectID} >
+  }
+  return (
+    <div className='Results-Container'>
+      {list.filter(isSearched(pattern)).map(item =>
+        <div className='Results-Details' key={item.objectID} >
 
-            <div>
-              <img src={require(`${item.image}`)} alt={`Movie Poster: ${item.title}`} />
-              <p>Movie Title : {item.title}</p>
-            </div>
-            <div>
-              <p>Director : {item.director}</p>
-            </div>
-            <span>
-              <Button
-                onClick={() => onDismiss(item.objectID)}
-                className='Button Dismiss'
-              >
-                Dismiss
-              </Button>
-              {/* <Button //show difference of being able to pass className as props instead
-                onClick={() => onDismiss(item.objectID)}
-                className='Button'
-              >
-                Dismiss
-              </Button> */}
-
-            </span>
-
-
+          <div>
+            <img src={require(`${item.image}`)} alt={`Movie Poster: ${item.title}`} />
+            <p>Movie Title : {item.title}</p>
           </div>
+          <div>
+            <p>Director : {item.director}</p>
+          </div>
+          <span>
+            <Button
+              onClick={() => onDismiss(item.objectID)}
+              className='Button Dismiss'
+            >
+              Dismiss
+            </Button>
+            {/* <Button //show difference of being able to pass className as props instead
+              onClick={() => onDismiss(item.objectID)}
+              className='Button'
+            >
+              Dismiss
+            </Button> */}
 
-        )}
+          </span>
 
-      </div>
-    )
-  }
+
+        </div>
+
+      )}
+
+    </div>
+  )
+
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props
-    return (
-      <form className='Search'>
-        <input
-          type='text'
-          value={value}
-          onChange={onChange}
+// class Search extends Component {
+//   render() {
+//     const { value, onChange, children } = this.props
+//     return (
+//       <form className='Search'>
+//         <input
+//           type='text'
+//           value={value}
+//           onChange={onChange}
 
-        ></input>
-        {children}
-      </form>
+//         ></input>
+//         {children}
+//       </form>
 
-    )
-  }
-}
+//     )
+//   }
+// }
 
-class Button extends Component {
-  render() {
-    //leave className optional by assigning when destructuring will override if there is a prop of className assigned within the component when its instantiated
-    const { onClick, className = '', children } = this.props;
-    return (
-      <button onClick={onClick}
-        className={className}
-        type='button'>
-        {children}
-      </button>
-    )
-  }
-}
+// function Search({ value, onChange, children }){
+//   return(
+//     <form className='Search'>
+//     <input
+//       type='text'
+//       value={value}
+//       onChange={onChange}
+
+//     ></input>
+//     {children}
+//   </form>
+//   )
+
+// }
+
+//changing to arrow function, remove block body, is now a concise body and the return is attached
+const Search = ({ value, onChange, children }) =>
+  //this.props.value, const {value} = this.props also is {value} when passed as argument it is automatically destructed for you and considered a prop
+  //{ is wanting to use anything in between like condition etc use curly brackets and return (jsx here)
+
+  <form className='Search'>
+    <input
+      type='text'
+      value={value}
+      onChange={onChange}
+
+    ></input>
+    {children}
+  </form>
+
+//}
+
+
+
+
+// class Button extends Component {
+//   render() {
+//     //leave className optional by assigning when destructuring will override if there is a prop of className assigned within the component when its instantiated
+//     const { onClick, className = '', children } = this.props;
+//     return (
+//       <button onClick={onClick}
+//         className={className}
+//         type='button'>
+//         {children}
+//       </button>
+//     )
+//   }
+// }
+
+
+//reminder instead of accessing with this.props and passing props we're destructuring within the parameter, the arguments are being destructured to access the props
+const Button = ({ onClick, className = '', children }) =>
+  <button onClick={onClick}
+    className={className}
+    type='button'>
+    {children}
+  </button>
 
 
 export default App;
