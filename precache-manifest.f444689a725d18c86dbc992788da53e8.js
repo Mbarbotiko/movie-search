275bc0209f82c9118c8b5f995a36186c9d31a7fd
Margarita Barbotiko <mbarbotiko@gmail.com>
2020-06-21 09:21:25 -0500

    Updates

---
+++ precache-manifest.f444689a725d18c86dbc992788da53e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74070a2412c77cf3a3456bdcf36411ee",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "7d71b19f786cffeb9e68",
+    "url": "/movie-search/static/css/main.f1f51edf.chunk.css"
+  },
+  {
+    "revision": "09d44e5340ef7fb2d4d6",
+    "url": "/movie-search/static/js/2.37807508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.37807508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d71b19f786cffeb9e68",
+    "url": "/movie-search/static/js/main.3804ad1e.chunk.js"
+  },
+  {
+    "revision": "8f1314dfdc21c95c9344",
+    "url": "/movie-search/static/js/runtime-main.8345f5b5.js"
+  },
+  {
+    "revision": "e46370da3a246d3c3a180f53102bb369",
+    "url": "/movie-search/static/media/diehard.e46370da.jpg"
+  },
+  {
+    "revision": "c8e1d5c47c89d59b43e7d39ab3a56929",
+    "url": "/movie-search/static/media/gremlins.c8e1d5c4.jpg"
+  },
+  {
+    "revision": "7c3c74dcba2c4163cd4f403f7f2febad",
+    "url": "/movie-search/static/media/jaws.7c3c74dc.jpg"
+  }
+]);
